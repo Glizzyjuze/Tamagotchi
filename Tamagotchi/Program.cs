d7a1cd93377d350948d41871c3592b08777a1f96
Hugo Zingmark <hugo.priebezingmark2@elev.ga.ntig.se>
2023-11-13 10:02:54 +0100

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,27 @@
+using System.Globalization;
+
+Tamagotchi tama = new Tamagotchi();
+
+tama.Printstats();
+
+// Console.WriteLine("Hi! What is my name?");
+// tama.name = Console.ReadLine();
+
+// while (tama.name == "")
+// {
+//     Console.WriteLine("Give me a name!");
+//     tama.name = Console.ReadLine();
+// }
+
+// Console.WriteLine($"{tama.name}. What a nice name!");
+// Console.ReadLine();
+
+// Console.WriteLine
+// (
+//     $@"{}
+//     press T to teach your tamagotchi a new word.
+//     press F to feed your tamagotchi.
+//     press H to say hi to your tamagotchi."
+// );
+
+Console.ReadLine();
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,39 @@
+﻿public class Tamagotchi
+{
+    private int hunger = 5;
+    private int boredom = 5;
+    private List<string> words;
+    private bool isAlive;
+    public string name;
+    public void Feed(int foodAmount)
+    {
+
+    }
+    public void Hi()
+    {
+
+    }
+    public void Teach(string word)
+    {
+
+    }
+    public void Tick()
+    {
+
+    }
+    public void Printstats()
+    {
+        Console.WriteLine($@"
+        {name} 
+        Hunger: {hunger}
+        Boredom: {boredom}");
+    }
+    public bool GetAlive()
+    {
+        return true;
+    }
+    private void ReduceBoredom()
+    {
+        
+    }
+}
